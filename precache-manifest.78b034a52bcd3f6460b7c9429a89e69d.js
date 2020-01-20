04bf00c6ab8d9e4307077fdfe450d07ad9b01dc5
Cisino Junior <cisinojr@gmail.com>
2020-01-20 13:04:09 -0300

    Updates

---
+++ precache-manifest.78b034a52bcd3f6460b7c9429a89e69d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85146465ab39d0c97752e959b35ace58",
+    "url": "/nearestdevs-web/index.html"
+  },
+  {
+    "revision": "194ef1b7a577419418c1",
+    "url": "/nearestdevs-web/static/css/main.eeab48ed.chunk.css"
+  },
+  {
+    "revision": "6f3fc4b19765f6e05552",
+    "url": "/nearestdevs-web/static/js/2.364e6f66.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nearestdevs-web/static/js/2.364e6f66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "194ef1b7a577419418c1",
+    "url": "/nearestdevs-web/static/js/main.2cd557cc.chunk.js"
+  },
+  {
+    "revision": "5a2926148ada54459fb7",
+    "url": "/nearestdevs-web/static/js/runtime-main.67b3a95a.js"
+  }
+]);
